ca792e6c6a3f989b808b7d41147addb1e4a466d4
hAbuMustafa <hosam5553@gmail.com>
2023-08-04 21:24:41 +0300

    Done with dad jokes api

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,13 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const $$$ = (selector) => Array.from($$(selector));
+function on(element, event, func) {
+  element.addEventListener(event, func);
+}
+
+const jButton = $('.getJoke');
+const jokeEl = $('.joke p');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +18,26 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'application/json' },
+  });
+  return res.json();
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+
+  if (item === not) return randomItemFromArray(arr, not);
+
+  return item;
+}
+
+async function handleClick(e) {
+  const { joke } = await fetchJoke();
+  jokeEl.textContent = joke;
+  jButton.textContent = randomItemFromArray(buttonText, jButton.textContent);
+}
+
+on(jButton, 'click', handleClick);
